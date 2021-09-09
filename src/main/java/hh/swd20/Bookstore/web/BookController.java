38dc925dd0007b015a75d2290d7de33f73e1a4d0
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2021-09-09 15:02:11 +0300

    index.html added

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,13 +1,16 @@
 package hh.swd20.Bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
 	
 	@GetMapping("/index")
-	
-	
+	public String getIndex(Model model) {
+		return "index";	
+	}
+		
 
 }
