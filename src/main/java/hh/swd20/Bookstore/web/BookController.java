0302b9d651592afb7311b73ef0a70c5b3839c731
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2021-09-17 22:01:48 +0300

    exC3.2

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,15 +1,24 @@
 package hh.swd20.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.swd20.Bookstore.domain.BookRepository;
+
 
 @Controller
 public class BookController {
 	
-	@GetMapping("/index")
-	public String getIndex(Model model) {
-		return "index";	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@GetMapping("/booklist")
+	public String getAllBooks(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist";	
 	}
 		
 
