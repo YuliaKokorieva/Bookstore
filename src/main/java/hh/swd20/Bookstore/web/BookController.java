c13fcca185083f3bd88ee2d9c2e43d5da56afd10
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2021-09-20 23:44:34 +0300

    exC3.4+C3.5

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
@@ -27,20 +26,36 @@ public class BookController {
 	@RequestMapping(value = "/addbook")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
+		model.addAttribute("header", "Add a new book");
 		return "addbook";
 	}
 	
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@RequestMapping(value = "/save")
 	public String saveBook(Book book) {
 		bookRepository.save(book);
-		return "redirect:booklist";
+		return "redirect:../booklist";
 	}
 	
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+	@RequestMapping(value = "/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId) {
 		bookRepository.deleteById(bookId);
 		return "redirect:/booklist";
 	}
+	
+	@RequestMapping(value = "/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", bookRepository.findById(bookId));
+		model.addAttribute("header", "Edit book");
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/edit/save")
+	public String saveEditedBook(Book book) {
+		bookRepository.save(book);
+		return "redirect:../booklist";
+	}
+	
+		
 	
 		
 
